960d067c1251c7303bc91b9f6a2dc9f0904e58ff
alvisAMD <as@sed.g>
2016-06-09 23:18:53 +0300

    изменения на ветке main

--- src/main/java/servlets/MyServlet.java
+++ src/main/java/servlets/MyServlet.java
@@ -24,7 +24,7 @@ public class MyServlet extends HttpServlet {
         String varTextA = "Hello World!";
         request.setAttribute("textA", varTextA);
         String varTextB = "It JSP.";
-        request.setAttribute("textB", varTextB);
+        request.setAttribute("textB_main", varTextB);
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
         dispatcher.forward(request, response);
